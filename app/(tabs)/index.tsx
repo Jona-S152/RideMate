b2b286dc045e2e361b5b712b9fd2cef7c75819fb
Jonathan <solisjonathan152@gmail.com>
2025-08-13 17:34:54 -0500

    Login

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,63 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { BlurView } from 'expo-blur';
+import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import { StatusBar, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <LinearGradient
+      start={{ x: 0.8, y: 0.2 }}
+      end={{ x: 0.5, y: 1.0 }}
+      locations={[0.3, 1]}
+      colors={["rgba(53, 35, 26, 1)", "rgba(105, 90, 82, 0.89) " ]}
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+    >
+      {/* Liquid Glass */}
+      <BlurView intensity={50} tint="light" style={{
+        position: 'absolute',
+        height: 525,
+        width: 160,
+        borderWidth: 1,
+        borderColor: 'rgba(255, 255, 255, 0.93)',
+        shadowColor: 'rgba(0, 0, 0, 1)',
+        shadowOffset: { width: 10, height: 10 },
+        shadowOpacity: 0.2,
+        shadowRadius: 20,
+        borderTopLeftRadius: 30,
+        borderBottomEndRadius: 30,
+        borderTopRightRadius: 200,
+        borderBottomLeftRadius: 200,
+        borderBottomRightRadius: 30,
+        transform: [{ scaleX: 2.0 }],
+        overflow: 'hidden',
+      }}>
+        <LinearGradient
+          start={{ x: 0.8, y: 0.2 }}
+          end={{ x: 0.5, y: 1.0 }}
+          locations={[0.1, 0.9]}
+          colors={["rgba(255, 255, 255, 0.05)", "rgba(255,255,255,0.1)"]}
+          style={{ flex: 1 }}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+
+      </BlurView>
+
+      <View style={{ position: 'relative', alignItems: 'center' }}>
+        <Text style={{ color: '#fff', fontSize: 50, fontWeight: 'bold', marginBottom: 10}}>
+          RideMate
+        </Text>
+        <TextInput placeholder='Correo electrónico' placeholderTextColor={'white'} style={{ shadowColor: '#8f8f8fff', height: 50}}/>
+        <TouchableOpacity style={{ backgroundColor: '#95514F', borderRadius: 10, padding: 10}}>
+          <Text style={{ color: 'white' }}>
+            Iniciar sesion
+          </Text>
+        </TouchableOpacity>
+        {/* <Button title="Presioname" onPress={() => alert('¡Click!')} /> */}
+      </View>
+
+      <StatusBar translucent backgroundColor="transparent" />
+    </LinearGradient>
   );
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+
